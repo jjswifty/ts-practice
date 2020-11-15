a64b943cf1d66a08287a82783ecf9c147aa52c32
jjswift <supercheater2018@yandex.ru>
2020-11-15 07:51:43 +0300

    Animal interface, and some implements

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
-(():void => {
-    console.log('bruhaasasasasda')
-})()
+interface Animal {
+    moving : string,
+    speech ?: boolean,
+    sex : string,
+    about : () => string,
+}
+
+class Cat implements Animal {
+    moving : string = 'ground'
+    speech : boolean
+    sex : string
+
+    constructor(sex: string, speech: boolean) {
+        this.speech = speech
+        this.sex = sex
+    }
+
+    about = () : string => (
+        `This animal ${this.speech ? 'can speak' : 'can\'t speak'}, moving on ${this.moving} and it's ${this.sex}`
+    )
+}
+
+class Bird implements Animal {
+	moving : string = 'sky'
+	speech : boolean
+	sex : string 
+
+	constructor(sex: string, speech: boolean) {
+		this.sex = sex
+		this.speech = speech
+	}
+
+	about = () : string => (
+		`This animal ${this.speech ? 'can speak' : 'can\'t speak'}, moving on ${this.moving} and it's ${this.sex}`
+	)
+}
+
+
+const Vasya = new Cat('female', true)
+const Kesha = new Bird('male', true)
+
+console.log(Vasya.about(), Kesha.about())
